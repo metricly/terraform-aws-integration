aa94235b1f832c772cbee1f2d3e71d5574b12da0
immortalsmoke <jennings.treutel@metricly.com>
2019-03-12 08:47:16 -0500

    Added template data source; updated iam_role resource.

--- metricly-integration.tf
+++ metricly-integration.tf
@@ -1,5 +1,12 @@
 # 
 
+data "template_file" "metricly_ro_policy" {
+  template = "${file("iam/metricly-ro-policy.json.tpl")}" 
+  vars = {
+    tpl_detailed_billing_bucket = "${var.detailed_billing_bucket}"
+  }
+}
+
 resource "aws_iam_role" "metricly" {
   name 			          = "TFMetriclyReadOnly"
   assume_role_policy 	= "${file("iam/assume-role.json")}"
@@ -8,5 +15,5 @@ resource "aws_iam_role" "metricly" {
 resource "aws_iam_role_policy" "metricly_ro_policy" {
   name   	= "TFMetriclyReadOnlyPolicy"
   role 		= "${aws_iam_role.metricly.id}" 
-  policy	= "${file("iam/metricly-ro-policy.json")}"
+  policy	= "${data.template_file.metricly_ro_policy.rendered}"
 }
